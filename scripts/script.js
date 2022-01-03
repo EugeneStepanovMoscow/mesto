48e798de8375766201b8937bf01a9d6bfe48ad7c
st.Eugene <st_eugene@mail.ru>
2022-01-03 18:47:46 +0300

    Изменения после первой проверки

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,14 @@ let overlay = document.querySelector('.overlay');
 let profilePersonName = document.querySelector('.profile__person-name');
 let profilePersonDesc = document.querySelector('.profile__person-description');
 let btnEdit = document.querySelector('.profile__btn-edit');
-let btnAdd = document.querySelector('.profile__btn-add');
 
 let btnPopupClose = document.querySelector('.popup__btn-close');
-let inpPopupPersonName = document.querySelector('.popup__inp_info_name');
-let inpPopupPersonDesc = document.querySelector('.popup__inp_info_description')
+let inpPopup = document.querySelectorAll('.popup__inp')
+let inpPopupPersonName = inpPopup[0];
+let inpPopupPersonDesc = inpPopup[1];
 let btnPopupSave = document.querySelector('.popup__btn-save');
+
+let formElement = document.querySelector('.popup');
 
 // Загрузка попапа
 function overlayActivated() {
@@ -16,12 +18,13 @@ function overlayActivated() {
   inpPopupPersonName.value = profilePersonName.textContent
   inpPopupPersonDesc.value = profilePersonDesc.textContent
 };
-
 function overlayDeactivated() {
   overlay.classList.remove('overlay_on')
 };
 
-function infoSave() {
+
+function infoSave(evt) {
+  evt.preventDefault()
   profilePersonName.textContent = inpPopupPersonName.value
   profilePersonDesc.textContent = inpPopupPersonDesc.value
   overlayDeactivated()
@@ -30,18 +33,7 @@ function infoSave() {
 btnEdit.addEventListener('click', overlayActivated);
 btnPopupClose.addEventListener('click', overlayDeactivated);
 btnPopupSave.addEventListener('click', infoSave);
-
-document.addEventListener('keydown', function(event) {
-  if (event.code === 'Escape') {
-    overlayDeactivated()
-  }
-});
-document.addEventListener('keydown', function(event) {
-  if (event.code === 'Enter') {
-    infoSave()
-  }
-});
-
+formElement.addEventListener('submit', infoSave);
 
 
 
