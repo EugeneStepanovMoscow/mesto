48fb313130e112f84c54535117baca4798ce4fd4
st.Eugene <st_eugene@mail.ru>
2022-01-04 23:26:26 +0300

    изменения после второй проверки

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,12 @@ let profilePersonDesc = document.querySelector('.profile__person-description');
 let btnEdit = document.querySelector('.profile__btn-edit');
 
 let btnPopupClose = document.querySelector('.popup__btn-close');
-let inpPopup = document.querySelectorAll('.popup__inp')
-let inpPopupPersonName = inpPopup[0];
-let inpPopupPersonDesc = inpPopup[1];
+//Не совсем понятно почему я не могу отставить выбор по форме let inpPopupPersonName = inpPopup[0];
+//т.к. при добавлении инпутов так же необходимо будет корректировать скрипт.
+//если инпуты дабавляются ниже, то просто добавляем переменны
+//если выше, то также добавляем переменные и корректруем порядок элементов inpPopup[0, 1, 2, и т.д.]
+let inpPopupPersonName = document.querySelector('input[name="name"]');
+let inpPopupPersonDesc = document.querySelector('input[name="description"]');
 let btnPopupSave = document.querySelector('.popup__btn-save');
 
 let formElement = document.querySelector('.popup');
@@ -33,7 +36,6 @@ function infoSave(evt) {
 btnEdit.addEventListener('click', overlayActivated);
 btnPopupClose.addEventListener('click', overlayDeactivated);
 btnPopupSave.addEventListener('click', infoSave);
-formElement.addEventListener('submit', infoSave);
 
 
 
