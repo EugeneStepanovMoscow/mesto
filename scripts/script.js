64a4ae416d41953e2a89d79649dbd755a1131387
st.Eugene <st_eugene@mail.ru>
2022-01-05 16:42:44 +0300

    исправления после проверки итерация3

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,11 @@ let profilePersonDesc = document.querySelector('.profile__person-description');
 let btnEdit = document.querySelector('.profile__btn-edit');
 
 let btnPopupClose = document.querySelector('.popup__btn-close');
-//Не совсем понятно почему я не могу отставить выбор по форме let inpPopupPersonName = inpPopup[0];
-//т.к. при добавлении инпутов так же необходимо будет корректировать скрипт.
-//если инпуты дабавляются ниже, то просто добавляем переменны
-//если выше, то также добавляем переменные и корректруем порядок элементов inpPopup[0, 1, 2, и т.д.]
 let inpPopupPersonName = document.querySelector('input[name="name"]');
 let inpPopupPersonDesc = document.querySelector('input[name="description"]');
 let btnPopupSave = document.querySelector('.popup__btn-save');
 
-let formElement = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
 
 // Загрузка попапа
 function overlayActivated() {
@@ -25,7 +21,6 @@ function overlayDeactivated() {
   overlay.classList.remove('overlay_on')
 };
 
-
 function infoSave(evt) {
   evt.preventDefault()
   profilePersonName.textContent = inpPopupPersonName.value
@@ -35,8 +30,7 @@ function infoSave(evt) {
 
 btnEdit.addEventListener('click', overlayActivated);
 btnPopupClose.addEventListener('click', overlayDeactivated);
-btnPopupSave.addEventListener('click', infoSave);
-
+formElement.addEventListener('submit', infoSave);
 
 
 
