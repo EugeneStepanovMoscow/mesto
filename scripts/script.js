2075cdcb974430db2436a65edf4ccc34f5d704ed
st.Eugene <st_eugene@mail.ru>
2022-01-06 18:44:31 +0300

    реализовано плавное открытие и закрытие попапа

--- scripts/script.js
+++ scripts/script.js
@@ -3,34 +3,114 @@ let overlay = document.querySelector('.overlay');
 let profilePersonName = document.querySelector('.profile__person-name');
 let profilePersonDesc = document.querySelector('.profile__person-description');
 let btnEdit = document.querySelector('.profile__btn-edit');
+let btnPlaceAdd = document.querySelector('.profile__btn-add')
 
-let btnPopupClose = document.querySelector('.popup__btn-close');
+let popupTitle = document.querySelector('.popup__title');
+let formElement = document.querySelector('.popup__form');
 let inpPopupPersonName = document.querySelector('input[name="name"]');
 let inpPopupPersonDesc = document.querySelector('input[name="description"]');
 let btnPopupSave = document.querySelector('.popup__btn-save');
+let btnPopupClose = document.querySelector('.popup__btn-close');
 
-let formElement = document.querySelector('.popup__form');
+let placesTable = document.querySelector('.places__table')
 
-// Загрузка попапа
-function overlayActivated() {
+const initialPlaces = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Загрузка попапа редактирования профиля
+function popupPersonEdit() {
   overlay.classList.add('overlay_on')
+  popupTitle.textContent = 'Редактирование профиля'
+  btnPopupSave.textContent = 'Сохранить'
   inpPopupPersonName.value = profilePersonName.textContent
   inpPopupPersonDesc.value = profilePersonDesc.textContent
 };
+// Загрузка попапа добавления карточки места
+function popupPlaceAdd() {
+  overlay.classList.add('overlay_on')
+  popupTitle.textContent = 'Новое место'
+  btnPopupSave.textContent = 'Создать'
+  inpPopupPersonName.value = 'Название'
+  inpPopupPersonDesc.value = 'Ссылка на картинку'
+};
+
+
+//___________________________________Функция добавления новых карточек
+function addPlace(placeName, placeImage) {
+  const placeTemplate = document.querySelector('#place-template').content
+  const placeNew = placeTemplate.querySelector('.place').cloneNode(true)
+
+  placeNew.querySelector('.place__name').textContent = placeName
+  placeNew.querySelector('.place__image').src = placeImage
+  placeNew.querySelector('.place__image').alt = placeName
+
+  // songElement.querySelector('.song__like').addEventListener('click', function(evt) {
+  //   evt.target.classList.toggle('song__like_active')
+  //   console.log(evt.target.classList)
+  // })
+
+  placesTable.prepend(placeNew)
+}
+//___________________________________Функция загрузки базовых карточек
+function placesInit() {
+  for (i = 3; i < initialPlaces.length; i++) {
+    addPlace(initialPlaces[i].name, initialPlaces[i].link)
+  }
+}
+
+
 function overlayDeactivated() {
   overlay.classList.remove('overlay_on')
 };
 
+//___________________________________Функция отработки submit на попапе
 function infoSave(evt) {
   evt.preventDefault()
-  profilePersonName.textContent = inpPopupPersonName.value
-  profilePersonDesc.textContent = inpPopupPersonDesc.value
-  overlayDeactivated()
+  if (popupTitle.textContent === 'Новое место') {  //на форме добавления места//
+    addPlace(inpPopupPersonName.value, inpPopupPersonDesc.value)
+    overlayDeactivated()
+  }
+  else {
+    profilePersonName.textContent = inpPopupPersonName.value  //на форме редактирования профиля//
+    profilePersonDesc.textContent = inpPopupPersonDesc.value
+    overlayDeactivated()
+  }
 };
 
-btnEdit.addEventListener('click', overlayActivated);
-btnPopupClose.addEventListener('click', overlayDeactivated);
+//загрузка базовых карточек
+placesInit()
+
+btnEdit.addEventListener('click', popupPersonEdit);
+btnPlaceAdd.addEventListener('click', popupPlaceAdd);
+
+btnPopupClose.addEventListener('click', overlayDeactivated); //попробовать вписать функцию короче в тело слушателя//
 formElement.addEventListener('submit', infoSave);
+
+
 
 
 
